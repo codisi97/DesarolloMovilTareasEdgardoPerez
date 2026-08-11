40062eb6880c4968c0e89db587cc08d4e510eaab
EAPerez007 <157754604+codisi97@users.noreply.github.com>
2026-08-10 20:49:40 -0600

    Primer commit context api

--- Context-API/App.tsx
+++ Context-API/App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CalculateExercises from './Components/calculateExercises';
 
 export default function App() {
-  return <CalculateExercises />
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
